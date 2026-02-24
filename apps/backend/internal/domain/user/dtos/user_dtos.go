64be2c69008fa78c969f49e4381384dce00d1913
Jaden Sooklal <jadensooklal5677@gmail.com>
2026-02-23 23:11:43 -0400

    [HDR-15] Did a draft of user_dtos.go

---
+++ apps/backend/internal/domain/user/dtos/user_dtos.go
@@ -0,0 +1,54 @@
+package dtos
+
+import (
+	"github.com/HDR3604/HelpDeskApp/internal/domain/user/aggregate"
+)
+
+type CreateUserRequest struct {
+	Email    string `json:"email"`
+	Password string `json:"Password"`
+	Role     string `json:"Role"`
+}
+
+type UpdateUserRequest struct {
+	Email    *string `json:"email"`
+	Role     *string `json:"role"`
+	IsActive *bool   `json:"is_active"`
+}
+
+type UserResponse struct {
+	UserID          string `json:"user_id"`
+	Email           string `json:"email"`
+	Role            string `json:"role"`
+	IsActive        bool   `json:"is_active"`
+	CreatedAt       string `json:"created_at"`
+	UpdatedAt       string `json:"updated_at"`
+	EmailVerifiedAt string `json:"email_verified_at"`
+}
+
+func UserToResponse(u *aggregate.User) UserResponse {
+	resp := UserResponse{
+		UserID:    u.ID.String(),
+		Email:     u.Email,
+		Role:      u.ToModel().Role.String(),
+		IsActive:  u.IsActive,
+		CreatedAt: u.CreatedAt.Format("2006-01-02 15:04:02"),
+	}
+	if u.EmailVerifiedAt != nil {
+		formatted := u.EmailVerifiedAt.Format("2006-01-02 15:04:02")
+		resp.EmailVerifiedAt = formatted
+	}
+	if u.UpdatedAt != nil {
+		formatted := u.UpdatedAt.Format("2006-01-02 15:04:02")
+		resp.UpdatedAt = formatted
+	}
+	return resp
+}
+
+func UsersToResponse(users []*aggregate.User) []UserResponse {
+	responses := make([]UserResponse, len(users))
+	for i, u := range users {
+		responses[i] = UserToResponse(u)
+	}
+	return responses
+}
